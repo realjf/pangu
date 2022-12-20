33743de8574a5ec024398fa7f1d2eb7367945bf6
realjf <real_jf@hotmail.com>
2022-12-20 18:22:18 +0800

    update

--- cmake/easy_profiler.cmake
+++ cmake/easy_profiler.cmake
@@ -4,3 +4,11 @@ set(EASY_PROFILER_NO_SAMPLES ON CACHE BOOL "")
 
 add_subdirectory(deps/easy_sources)
 
+if(BUILD_WITH_EASY_PROFILER)
+    message("Enabled EasyProfiler")
+    add_definitions(-DBUILD_WITH_EASY_PROFILER=1)
+    add_definitions(-DCMAKE_BUILD_TYPE="Release")
+    include_directories(deps/easy_profiler/include)
+    set_property(TARGET easy_profiler PROPERTY FOLDER "ThirdPartyLibraries")
+    set_property(TARGET profiler_converter PROPERTY FOLDER "ThirdPartyLibraries")
+endif()
--- cmake/optick.cmake
+++ cmake/optick.cmake
@@ -4,4 +4,8 @@ if(BUILD_WITH_OPTICK)
     add_subdirectory(deps/optick)
 endif()
 
-
+if(BUILD_WITH_OPTICK)
+    message("Enabled Optick")
+    add_definitions(-DBUILD_WITH_OPTICK=1)
+    set_property(TARGET OptickCore PROPERTY FOLDER "ThirdPartyLibraries")
+endif()
